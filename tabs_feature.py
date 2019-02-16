8bde6034f40d8263d2a7b2b51c229552aa89612d
Leon Kvež <47056265+letter28@users.noreply.github.com>
2019-02-16 13:00:20 +0100

    Initial version of the app with the tabs feature working.

---
+++ tabs_feature.py
@@ -0,0 +1,54 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 10 20:22:03 2019
+
+@author: zero-
+"""
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.config['suppress_callback_exceptions']=True
+
+app.layout = html.Div([
+    html.H1('Fotonaponska elektrana FN1-Riteh: Podaci o proizvodnji'),
+    dcc.Tabs(id="my-tabs", value='tab-1-example', children=[
+        dcc.Tab(label='Grafički prikaz trenutačne proizvodnje 1', value='tab-1'),
+        dcc.Tab(label='Tablica s podacima o proizvodnji', value='tab-2'),
+    ]),
+    html.Div(id='tabs-content')
+])
+
+
+@app.callback(Output('tabs-content', 'children'),
+              [Input('my-tabs', 'value')])
+def render_content(tab):
+    if tab == 'tab-1':
+        return html.Div([
+            html.H3('Grafički prikaz trenutačne proizvodnje'),
+            dcc.Graph(
+                id='graph-1-tabs',
+                figure={
+                    'data': [{
+                        'x': [1, 2, 3],
+                        'y': [3, 1, 2],
+                        'type': 'line',
+                        'name': 'Snaga AC [W]'
+                    }]
+                }
+            )
+        ])
+    elif tab == 'tab-2':
+        return html.Div([
+            html.H3('Podaci o prethodnoj proizvodnji')
+                        ])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
